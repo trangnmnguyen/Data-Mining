ed2852d9b751f4366cdae41d57ffc7b3cb580774
Trang Nguyen <trangnmnguyen@gmail.com>
2018-07-04 16:35:12 -0400

    Complete Data Mining course

--- CART.R
+++ CART.R
@@ -1,91 +1,107 @@
-###regression tree
-#preparation
+### Lab6 CART 
+# Regression Tree 
 library(MASS)
 data(Boston)
-index <- sample(nrow(Boston),nrow(Boston)*0.90)
-boston.train <- Boston[index,]
-boston.test <- Boston[-index,]
-
-install.packages('rpart')
-install.packages('rpart.plot') 
+dim(Boston)
+str(Boston)
+install.packages("rpart")
+install.packages("rpart.plot")
 library(rpart)
 library(rpart.plot)
 
-#regression tree
-boston.rpart <- rpart(formula = medv ~ ., data = boston.train)
-boston.rpart
-prp(boston.rpart, digits = 4, extra = 1)
+# training and testing 
+index = sample(nrow(Boston),nrow(Boston)*0.8)
+train.boston = Boston[index,]
+test.boston = Boston[-index,]
 
-boston.train.pred.tree = predict(boston.rpart)
-boston.test.pred.tree = predict(boston.rpart, newdata=boston.test)
+boston.tree1=rpart(medv~., data=train.boston)
+#deviance is SSE , yval is the average predicted value in that particular node 
+prp(boston.tree1, digits = 4, extra =1)
 
-###classification tree
-#preparation
+#prediction
+boston.pred.train=predict(boston.tree1)
+boston.pred.test=predict(boston.tree1,newdata=test.boston)
+#MSPE 
+mean((test.boston$medv-boston.pred.test)^2)
+
+#excercise compare with linear regression 
+boston.lm=lm(medv~.,data=train.boston)
+pred.lm.test=predict(boston.lm,newdata=test.boston)
+mean((test.boston$medv-pred.lm.test)^2)
+
+cv.data.lm=glm(medv~.,data=train.boston)
+cv.glm(train.boston, cv.data.lm, K=10)$delta[2]
+# Nsplit+1=size of tree 
+##prune the tree 
+#smaller value of cp makes larger tree 
+boston.tree2=rpart(medv~., data=train.boston, cp=0.001)
+prp(boston.tree2, digits=4, extra=1)
+#dash line is smallest CV + 1 standard error 
+#the most important variable of a tree is the top variable in which the tree first split 
+plotcp(boston.tree2)
+printcp(boston.tree2)
+#The relative error is the relative to the ROOT NODE ERROR (RNE) => we have to take xerror * RNE  to get real CV 
+
+# CLASSIFICATION TREE
 credit.data <- read.csv("http://homepages.uc.edu/~lis6/DataMining/Data/credit_default.csv", header=T)
-# rename
-library(dplyr)
-credit.data<- rename(credit.data, default=default.payment.next.month)
+names(credit.data)
+names(credit.data)[24]="default"
+#convert sex, education and marriage to catergorial variable 
+credit.data$SEX=as.factor(credit.data$SEX) #change the type of the variable
+credit.data$EDUCATION=as.factor(credit.data$EDUCATION)
+credit.data$MARRIAGE=as.factor(credit.data$MARRIAGE)
 
-# convert categorical data to factor
-credit.data$SEX<- as.factor(credit.data$SEX)
-credit.data$EDUCATION<- as.factor(credit.data$EDUCATION)
-credit.data$MARRIAGE<- as.factor(credit.data$MARRIAGE)
+# split 
+index=sample(nrow(credit.data), 0.8*nrow(credit.data))
+credit.train=credit.data[index,]
+credit.test=credit.data[-index,]
 
-# random splitting
-index <- sample(nrow(credit.data),nrow(credit.data)*0.80)
-credit.train = credit.data[index,]
-credit.test = credit.data[-index,]
+#tree 
+credit.tree1=rpart(default~., data=credit.train, method = "class")
+prp(credit.tree1, digits =4, extra =1)
 
-#fitting classification tree
-credit.rpart0 <- rpart(formula = default ~ ., data = credit.train, method = "class")
-pred0<- predict(credit.rpart0, type="class")
-table(credit.train$default, pred0, dnn = c("True", "Pred"))]
-credit.rpart <- rpart(formula = default ~ . , data = credit.train, method = "class", parms = list(loss=matrix(c(0,5,1,0), nrow = 2)))
-credit.rpart
-prp(credit.rpart, extra = 1)
+#prediction 
+credit.pred1=predict(credit.tree1,type="class")
+credit.pred1.test=predict(credit.tree1, newdata=credit.test,type="class")
+#confusion matrix 
+table(credit.test$default, credit.pred1.test, dnn = c("TRUE", "PRED"))
+#MR
+mean(credit.test$default !=credit.pred1.test)
 
-#prediction using classification tree
-credit.train.pred.tree1<- predict(credit.rpart, credit.train, type="class")
-table(credit.train$default, credit.train.pred.tree1, dnn=c("Truth","Predicted"))
+#Excercise compare with logit model (symetric cost) (specific pcut=0.5 in the logistic model to compare it )
+#asymetric cost classificationt ree 
+credit.tree2=rpart(default~., data=credit.train, method = "class", 
+                   parms=list(loss=matrix(c(0,8,1,0), nrow=2)))
+prp(credit.tree2, digits =4, extra =1)
+#excercist prediction, WMR (get confuction matrix first then you need to calculate MR based on weight)
+#exercist pruen the tree 
 
-#Predicted Class
-cost <- function(r, pi){
-  weight1 = 5
-  weight0 = 1
-  c1 = (r==1)&(pi==0) #logical vector - true if actual 1 but predict 0
-  c0 = (r==0)&(pi==1) #logical vector - true if actual 0 but predict 1
-  return(mean(weight1*c1+weight0*c0))
-}
-cost(credit.train$default,credit.train.pred.tree1)
-
-#Fit logistic regression model
-credit.glm<- glm(default~., data = credit.train, family=binomial)
-#Get binary prediction
-credit.test.pred.glm<- as.numeric(predict(credit.glm, credit.test, type="response")>0.21)
-#Calculate cost using test set
-cost(credit.test$default,credit.test.pred.glm)
-#Confusion matrix
-table(credit.test$default, credit.test.pred.glm, dnn=c("Truth","Predicted"))
-
-credit.test.prob.rpart<- predict(credit.rpart,credit.test, type="prob")
-
+#ROC CURVE 
+credit.pred2=predict(credit.tree2)[,2]
 library(ROCR)
+pred=prediction(credit.pred2, credit.train$default)
+perf=performance(pred, "tpr","fpr")
+plot(perf,colorize=TRUE)
+#get AUC 
+unlist(slot(performance(pred,"auc"), "y.values"))
 
-pred = prediction(credit.test.prob.rpart[,2], credit.test$default) 
-perf = performance(pred, "tpr", "fpr")
-plot(perf, colorize=TRUE)
 
-#Get the AUC
-unlist(slot(performance(pred, "auc"), "y.values"))
 
-#Pruning
-boston.largetree <- rpart(formula = medv ~ ., data = boston.train, cp = 0.001)
-prp(boston.largetree)
-plotcp(boston.largetree)
-printcp(boston.largetree)
-sum((boston.train$medv - mean(boston.train$medv))^2)/nrow(boston.train)
-mean((predict(boston.largetree) - boston.train$medv)^2)
-prune(boston.largetree, cp = 0.008)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
